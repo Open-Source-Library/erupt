708bcec5b90cfd895c739c2d3dfd9465ebf30064
marj <ma19881117@163.com>
2021-07-20 00:31:56 +0000

    序列号正则表达式到前端

--- erupt-annotation/src/main/java/xyz/erupt/annotation/sub_field/sub_edit/InputType.java
+++ erupt-annotation/src/main/java/xyz/erupt/annotation/sub_field/sub_edit/InputType.java
@@ -2,7 +2,6 @@ package xyz.erupt.annotation.sub_field.sub_edit;
 
 import xyz.erupt.annotation.config.Comment;
 
-import java.beans.Transient;
 
 /**
  * @author YuePeng
@@ -18,7 +17,6 @@ public @interface InputType {
     @Comment("是否整行显示")
     boolean fullSpan() default false;
 
-    @Transient
     @Comment("对提交内容进行正则校验")
     String regex() default "";
 
