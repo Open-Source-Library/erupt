bd24bf584b8bb3c09bdbad75e15021797b13f45f
jiangxin <bloy@live.cn>
2022-09-11 13:28:39 +0800

    no message

--- erupt-core/src/main/java/xyz/erupt/core/service/EruptCoreService.java
+++ erupt-core/src/main/java/xyz/erupt/core/service/EruptCoreService.java
@@ -92,7 +92,11 @@ public class EruptCoreService implements ApplicationRunner {
                 .ifPresent(ignore -> {
                     EruptFieldModel eruptFieldModel = new EruptFieldModel(field);
                     eruptModel.getEruptFieldModels().add(eruptFieldModel);
+                    if(eruptModel.getEruptFieldMap().containsKey(field.getName())){
+
+                    }else{
                     eruptModel.getEruptFieldMap().put(field.getName(), eruptFieldModel);
+                    }
                 }));
         eruptModel.getEruptFieldModels().sort(Comparator.comparingInt((a) -> a.getEruptField().sort()));
         // erupt annotation validate
