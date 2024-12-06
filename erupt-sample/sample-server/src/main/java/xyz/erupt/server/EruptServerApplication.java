84f0df44686664f829e2a419ef16ca3c387e85d0
“barcke” <barcke.yu@ideamake.cn>
2024-12-06 18:01:07 +0800

    feature: 新增cloud-server 样本，实现 docker 部署功能

---
+++ erupt-sample/sample-server/src/main/java/xyz/erupt/server/EruptServerApplication.java
@@ -0,0 +1,22 @@
+package xyz.erupt.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Barcke
+ * @version 1.0
+ * @projectName erupt
+ * @className EruptServerApplication
+ * @date 2024/12/6 10:41
+ * @slogan: 源于生活 高于生活
+ * @description:
+ **/
+@SpringBootApplication
+public class EruptServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EruptServerApplication.class, args);
+    }
+
+}
